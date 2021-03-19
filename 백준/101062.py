dc591a294ec8d36ada027a9fdc3cacea51340d06
qkrtndh <qkrtndh5@naver.com>
2021-03-19 21:04:09 +0900

    1012번 추가

--- 백준/101062.py
+++ 백준/101062.py
@@ -1,29 +1,4 @@
-t = int(input())
-
-A = 300
-B = 60
-C = 10
-
-cnt = [0]*3
-result=1
-def divid(n,num):
-    global t
-    global cnt
-    while t//num>0:
-        cnt[n]+=t//num
-        t%=num
-
-while t>1:
-    if t%C!=0:
-        result = 0
-        break
-    if t//A>0:
-        divid(0,A)
-    if t//B>0:
-        divid(1,B)
-    if t//C>0:
-        divid(2,C)
-if result:
-    print(cnt[0],cnt[1],cnt[2])
-else:
-    print(-1)
+n,m = map(int,input().split())
+arr = list(map(int,input().split()))
+arr.sort()
+print(arr[m-1])
---
+++ 백준/1012.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(50000)
+def dfs(a,b):
+    brr[a][b]=1
+    for i in range(4):
+        x = dx[i]+b
+        y = dy[i]+a
+        if 0 <= x < m and 0 <= y < n and arr[y][x]==1 and brr[y][x]==0:
+            dfs(y,x)
+
+t = int(input())
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+for _ in range(t):
+    m,n,k = map(int,input().split())
+    arr = [[0]*m for _ in range(n)]
+    brr = [[0]*m for _ in range(n)]
+    for _ in range(k):
+        a,b = map(int,input().split())
+        arr[b][a] = 1
+    cnt =0
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] == 1 and brr[i][j]==0:
+                cnt+=1
+                dfs(i,j)
+    print(cnt)
