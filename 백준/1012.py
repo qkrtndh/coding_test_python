26d27be4e524bf0949ca8fd0285578e8e88d8f12
qkrtndh <qkrtndh5@naver.com>
2021-03-19 21:06:33 +0900

    1012번 수정

--- 백준/1012.py
+++ 백준/1012.py
@@ -1,11 +1,11 @@
 import sys
 sys.setrecursionlimit(50000)
 def dfs(a,b):
-    brr[a][b]=1
+    arr[a][b]=0
     for i in range(4):
         x = dx[i]+b
         y = dy[i]+a
-        if 0 <= x < m and 0 <= y < n and arr[y][x]==1 and brr[y][x]==0:
+        if 0 <= x < m and 0 <= y < n and arr[y][x]==1:
             dfs(y,x)
 
 t = int(input())
@@ -14,14 +14,13 @@ dy = [-1,1,0,0]
 for _ in range(t):
     m,n,k = map(int,input().split())
     arr = [[0]*m for _ in range(n)]
-    brr = [[0]*m for _ in range(n)]
     for _ in range(k):
         a,b = map(int,input().split())
         arr[b][a] = 1
     cnt =0
     for i in range(n):
         for j in range(m):
-            if arr[i][j] == 1 and brr[i][j]==0:
+            if arr[i][j] == 1:
                 cnt+=1
                 dfs(i,j)
     print(cnt)
