f620e8d3e0aba8aecac01e8d58f2b174a7a93eb5
qkrtndh <qkrtndh5@naver.com>
2021-03-23 17:55:21 +0900

    1461번 문제 미완

---
+++ 백준/1461.py
@@ -0,0 +1,14 @@
+n,m = map(int,input().split())
+arr = list(map(int,input().split()))
+plus,minus = [], []
+for i in range(n):
+    if arr[i]<0:
+        minus.append(arr[i])
+    else:
+        plus.append(arr[i])
+minus.sort()
+plus.sort()
+switch = 0
+if max(plus)<min(minus)*(-1):
+    if len(plus)%m!=0:
+        
