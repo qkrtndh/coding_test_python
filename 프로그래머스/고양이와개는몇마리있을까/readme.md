f68583fe928b24d87cf2c58aca484390a41d1b41
qkrtndh <qkrtndh5@naver.com>
2021-10-25 09:55:14 +0900

    고양이와개는 몇마리 있을까

---
+++ 프로그래머스/고양이와개는몇마리있을까/readme.md
@@ -0,0 +1,4 @@
+![image](https://user-images.githubusercontent.com/65153512/138619667-4b9e8852-1719-490d-88d2-c2ed42859540.png)
+
+
+SQL문 풀이 문제이다.
---
+++ 프로그래머스/고양이와개는몇마리있을까/고양이와개.sql
@@ -0,0 +1,1 @@
+SELECT ANIMAL_TYPE, count(*) as count from animal_ins where animal_type='Cat' or animal_type='Dog' group by animal_type order by ANIMAL_TYPE
